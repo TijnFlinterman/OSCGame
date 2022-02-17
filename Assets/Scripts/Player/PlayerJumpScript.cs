92e368c5986e1ca12ddbb859da45a76a03a27282
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-02-17 10:51:56 +0100

    script disabled, light moved and changed to spot light and changes scene settings

--- Assets/Scripts/Player/PlayerJumpScript.cs
+++ Assets/Scripts/Player/PlayerJumpScript.cs
@@ -1,19 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PlayerJumpScript : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
+//public class PlayerJumpScript : MonoBehaviour
+//{
+//    public Transform GroundCheck;
+//    public float groundDistance = 0.4f;
+//    public LayerMask groundMask;
 
-    void Update()
-    {
-      //  if (Input.GetKey(KeyCode.Space))
-        //{
-            
-        //}
-    }
-}
+//    bool isGrounded;
+//    public float gravity = -9.81f;
+//    public float jumpHeight = 3f;
+//    Vector3 velocity;
+
+//    void Update()
+//    {
+//        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+//        if (Input.GetButtonDown("Jump") && isGrounded)
+//        {
+//            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+//        }
+//    }
+//}
