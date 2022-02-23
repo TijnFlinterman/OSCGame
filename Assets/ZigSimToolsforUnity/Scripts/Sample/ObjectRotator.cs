f03827ecfc2abd19a12dfa9fb7b4bcee482b419d
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-02-23 12:30:57 +0100

    working OSC connection

---
+++ Assets/ZigSimToolsforUnity/Scripts/Sample/ObjectRotator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using ZigSimTools;
+using Quaternion = UnityEngine.Quaternion;
+
+public class ObjectRotator : MonoBehaviour
+{
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        ZigSimDataManager.Instance.StartReceiving();
+        ZigSimDataManager.Instance.QuaternionCallBack += (ZigSimTools.Quaternion q) =>
+        {
+            // Debug.Log (q.ToString ());
+            var newQut = new Quaternion(0, (float)-q.z, 0, 0);
+            var newRot = newQut * Quaternion.Euler(90f, 0, 0);
+            targetRotation = newRot;
+        };
+    }
+
+    void Update()
+    {
+        transform.localRotation = targetRotation;
+    }
+}
