d3ab156d050106fa8322cf070f2f8f769cebe034
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-02-23 12:58:59 +0100

    sample scene update

--- Assets/ZigSimToolsforUnity/Scripts/Sample/ObjectRotator.cs
+++ Assets/ZigSimToolsforUnity/Scripts/Sample/ObjectRotator.cs
@@ -6,20 +6,25 @@ public class ObjectRotator : MonoBehaviour
 {
     private Quaternion targetRotation;
 
-    void Start()
+    void Start ()
     {
-        ZigSimDataManager.Instance.StartReceiving();
+        ZigSimDataManager.Instance.StartReceiving ();
         ZigSimDataManager.Instance.QuaternionCallBack += (ZigSimTools.Quaternion q) =>
         {
             // Debug.Log (q.ToString ());
-            var newQut = new Quaternion(0, (float)-q.z, 0, 0);
-            var newRot = newQut * Quaternion.Euler(90f, 0, 0);
+            var newQut = new Quaternion ((float) - q.x, (float) - q.z, (float) - q.y, (float) q.w);
+            var newRot = newQut * Quaternion.Euler (90f, 0, 0);
             targetRotation = newRot;
         };
     }
 
-    void Update()
+    void Update ()
     {
         transform.localRotation = targetRotation;
+
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            ZigSimDataManager.Instance.StopReceiving ();
+        }
     }
 }
