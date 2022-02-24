36b0268a5be994403a9c46dbde18375abd9fb7fe
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-02-24 10:09:49 +0100

    gameplay + object rotater script

--- Assets/ZigSimToolsforUnity/Scripts/Sample/ObjectRotator.cs
+++ Assets/ZigSimToolsforUnity/Scripts/Sample/ObjectRotator.cs
@@ -5,19 +5,31 @@ using Quaternion = UnityEngine.Quaternion;
 public class ObjectRotator : MonoBehaviour
 {
     private Quaternion targetRotation;
+    private Vector3 test;
 
     void Start ()
     {
         ZigSimDataManager.Instance.StartReceiving ();
         ZigSimDataManager.Instance.QuaternionCallBack += (ZigSimTools.Quaternion q) =>
         {
-            Quaternion newRot = Quaternion.Euler (0, 0, (float)q.z * 180);
+            Quaternion newRot = Quaternion.Euler (0, 0, (float)q.x * -360);
             targetRotation = newRot;
+
         };
     }
 
     void Update ()
     {
         transform.localRotation = targetRotation;
+    }
+    public static Quaternion SmoothDampQuaternion(Quaternion current, Quaternion target, ref Vector3 currentVelocity, float smoothTime)
+    {
+        Vector3 c = current.eulerAngles;
+        Vector3 t = target.eulerAngles;
+        return Quaternion.Euler(
+          Mathf.SmoothDampAngle(c.x, t.x, ref currentVelocity.x, smoothTime),
+          Mathf.SmoothDampAngle(c.y, t.y, ref currentVelocity.y, smoothTime),
+          Mathf.SmoothDampAngle(c.z, t.z, ref currentVelocity.z, smoothTime)
+        );
     }
 }
